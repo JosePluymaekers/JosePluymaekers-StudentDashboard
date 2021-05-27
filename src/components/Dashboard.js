f2e3abd3fbfcc1ce03a78115aac5a84c5c6c8a76
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-05-27 20:41:35 +0200

    Add files via upload

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,62 +2,62 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 function Dashboard(props) {
-  const options = props.students.map((student) => {
-    return (
-      <option key={student.id} value={student.id}>
-        {student.firstName}
-      </option>
-    );
-  });
+	const options = props.students.map((student) => {
+		return (
+			<option key={student.id} value={student.firstName}>
+				{student.firstName}
+			</option>
+		);
+	});
 
-  const optionsAssignment = props.assignments.map((assignment) => {
-    return (
-      <option key={assignment.id} value={assignment.id}>
-        {assignment.name}
-      </option>
-    );
-  });
+	const optionsAssignment = props.assignments.map((assignment) => {
+		return (
+			<option key={assignment.id} value={assignment.id}>
+				{assignment.name}
+			</option>
+		);
+	});
 
-  const studentSelectHandleChange = (event) => {
-    props.history.push(`${event.target.value}`);
-  };
+	const studentSelectHandleChange = (event) => {
+		props.history.push(`${event.target.value}`);
+	};
 
-  const assignmentSelectHandleChange = (event) => {
-    props.history.push(`${event.target.value}`);
-    alert("helaas, deze werkt niet goed");
-  };
+	const assignmentSelectHandleChange = (event) => {
+		props.history.push(`${event.target.value}`);
+		alert("helaas, deze werkt niet goed");
+	};
 
-  const buttonHandleChange = (event) => {
-    props.history.push("/Container");
-  };
+	const buttonHandleChange = (event) => {
+		props.history.push("/Container");
+	};
 
-  return (
-    <div className="filter-div" style={{ position: "relative", zIndex: "2" }}>
-      <button id="selectAllStudents" onClick={buttonHandleChange}>
-        See all results
-      </button>
-      <label>
-        <select
-          name="select-student"
-          id="selectStudent"
-          onChange={studentSelectHandleChange}
-        >
-          <option value="container">Select single student</option>
-          {options}
-        </select>
-      </label>
-      <label>
-        <select
-          name="select-assignment"
-          id="selectAssignment"
-          onChange={assignmentSelectHandleChange}
-        >
-          <option value="container">Select single assignment</option>
-          {optionsAssignment}
-        </select>
-      </label>
-    </div>
-  );
+	return (
+		<div className="filter-div" style={{ position: "relative", zIndex: "2" }}>
+			<button id="selectAllStudents" onClick={buttonHandleChange}>
+				See all results
+			</button>
+			<label>
+				<select
+					name="select-student"
+					id="selectStudent"
+					onChange={studentSelectHandleChange}
+				>
+					<option value="container">Select single student</option>
+					{options}
+				</select>
+			</label>
+			<label>
+				<select
+					name="select-assignment"
+					id="selectAssignment"
+					onChange={assignmentSelectHandleChange}
+				>
+					<option value="container">Select single assignment</option>
+					{optionsAssignment}
+				</select>
+			</label>
+		</div>
+	);
 }
 
 export default withRouter(Dashboard);
--- src/components/Student.js
+++ src/components/Student.js
@@ -2,15 +2,15 @@ import React from "react";
 import Chart from "./Chart";
 
 function Student(props) {
-  return (
-    <div>
-      <Chart
-        graphData={props.data}
-        radioState={props.radioState}
-        handleFilterChange={props.handleFilterChange}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<Chart
+				graphData={props.data}
+				radioState={props.radioState}
+				handleFilterChange={props.handleFilterChange}
+			/>
+		</div>
+	);
 }
 
 export default Student;
